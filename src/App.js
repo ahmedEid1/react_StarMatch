bf84ad7a1e18a697d28e4fe9eff914935473dcdc
ahmedeid <kingmedo954@gmail.com>
2020-10-01 14:05:22 +0200

    adding numbers and stars to the ui based on the vars. provided

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const StarMatch = () => {
+  const [stars, setStars] = useState(utils.random(1, 9));
+  const numbers = 9;
+  return (
+      <div className="game">
+        <div className="help">
+          Pick 1 or more numbers that sum to the number of stars
+        </div>
+        <div className="body">
+          <div className="left">
+
+            {utils.range(1, stars).map(starId => <div id={starId} className="star" /> )}
+
+          </div>
+          <div className="right">
+            {utils.range(1, numbers).map(number =>
+                <button key={number} className="number">{number}</button>
+            )}
+
+          </div>
+        </div>
+        <div className="timer">Time Remaining: 10</div>
+      </div>
+  )
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StarMatch />
   );
 }
 
 export default App;
+
+
+// copied from the  starting code
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+
+
+const utils = {
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
+
