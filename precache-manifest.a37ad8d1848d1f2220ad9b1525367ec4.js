ae5badeab93e29e05e100bc575ffe495bc113c85
ahmed_eid <kingmedo954@gmail.com>
2021-01-11 19:29:28 +0200

    Updates

---
+++ precache-manifest.a37ad8d1848d1f2220ad9b1525367ec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708a211f7d8f26f619fa0b0ac8a827df",
+    "url": "/react_StarMatch/index.html"
+  },
+  {
+    "revision": "d0b0857eb5956645e877",
+    "url": "/react_StarMatch/static/css/main.44fe145c.chunk.css"
+  },
+  {
+    "revision": "c0ceec63efc3445ed972",
+    "url": "/react_StarMatch/static/js/2.e1ac8114.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_StarMatch/static/js/2.e1ac8114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b0857eb5956645e877",
+    "url": "/react_StarMatch/static/js/main.54cc7e49.chunk.js"
+  },
+  {
+    "revision": "cced7de200c6d279137b",
+    "url": "/react_StarMatch/static/js/runtime-main.46b523dc.js"
+  }
+]);
